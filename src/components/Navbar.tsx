8dc208c6afd1c4b096e1e244441522d7103ffd0c
Roy Ayala <troysg8203@gmail.com>
2024-10-25 13:30:11 -0400

    feate: navbar finished

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,23 @@
-import React from "react";
+import { IconSearch } from "@tabler/icons-react";
 
 export const Navbar = () => {
   return (
-    <div className="flex flex-row items-center justify-between px-5 py-7">
+    <div className="flex flex-col md:flex-row items-center  justify-center sm:justify-between px-5 py-7 flex-wrap gap-2">
       <a
         href="#"
         className="px-3 py-4 rounded-md hover:bg-white/5 cursor-pointer"
       >
         <h1 className="text-2xl font-bold">Error 404</h1>
       </a>
-      <div className="border-4 px-3 py-4 rounded-full border-white">
+      <div className="flex w-full md:w-auto  items-center justify-between border-2 md:border-4 px-5 py-4 rounded-full border-white">
         <input
           type="text"
           className="appearance-none border-none bg-transparent focus:outline-none placeholder:text-white/70"
           placeholder="Buscar..."
         />
-        <button>Hola</button>
+        <button>
+          <IconSearch />
+        </button>
       </div>
     </div>
   );
